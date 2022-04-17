cd49ff1e56840b660323af987b228e1d4e8a6ca2
pawan-v <pawan1512001@gmail.com>
2022-04-17 15:28:57 +0530

    2nd ques

---
+++ Arrays/pairSum.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+pair<int,int> pairSum(vector<int> arr,int target){
+    int n = arr.size();
+    unordered_set<int> s ;
+    pair<int,int> res ;
+
+    for(int i =0 ;i<n;i++){
+        int x = target - arr[i] ;
+        if(s.find(x) != s.end()){
+            res.first = arr[i];
+            res.second = x ;
+            break;
+        }
+        else{
+            s.insert(arr[i]) ;
+        }
+    }
+return res ;
+
+}
+
+ 
+int main() {
+ vector<int> a = {5,4,3,2,1};
+ int target = 7 ;
+  
+ pair<int,int> ans = pairSum(a,target) ;
+ cout<<ans.first<<" , "<<ans.second<<endl ;
+
+ 
+return 0  ;
+}
