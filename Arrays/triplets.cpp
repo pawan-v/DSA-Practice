8f44db04499ae38d5a2d07d156ca5cda3fd18082
pawan-v <pawan1512001@gmail.com>
2022-04-20 23:48:58 +0530

    main completed and done changes

--- Arrays/triplets.cpp
+++ Arrays/triplets.cpp
@@ -5,12 +5,11 @@ using namespace std ;
 vector<vector<int>> triplet(vector<int> arr,int target){
     int n = arr.size() ;
     vector<vector<int>> res ;
+    sort(arr.begin(),arr.end());
+
     for(int i =0 ;i<n-3;i++){
         int j = i+1 ;
         int k = n-1 ;
-
-       
-
         while (i<k)
         {
             int curr_sum = arr[i] ;
@@ -37,6 +36,20 @@ vector<vector<int>> triplet(vector<int> arr,int target){
  
 int main() {
  
+ vector<int> arr ={2,4,7,9,4,3,6,8,1,0} ;
+ int target = 7 ;
+ 
+ vector<vector<int>> ans ;
+ ans = triplet(arr,target);
+
+ for(auto x : ans){
+     cout<<"{ " ;
+     for(auto y : x){
+         cout<<y<<"," ;
+     }
+     cout<<"}"<<endl ;
+ }
+
  
 return 0  ;
 }
